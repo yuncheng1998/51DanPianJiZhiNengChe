4b3a0ba8e7cb6b71da0ff3bd754c24bc9eb333a4
fafnir <yuncheng1998@icloud.com>
2017-08-28 23:13:15 +0800

    优化

--- 循迹/head.h
+++ 循迹/head.h
@@ -4,21 +4,21 @@
 #define Left_1_led P3_7  //循迹模块
 #define Right_1_led P3_6
 
-#define Left_moto_pwm	  P1_6	 //PWM信号端
-#define Right_moto_pwm	  P1_7
+#define Left_moto_pwm     P1_6   //PWM信号端
+#define Right_moto_pwm    P1_7
 
 #define Left_moto_go      {P1_2=1,P1_3=0;}  //左电机向前走
-#define Left_moto_back    {P1_2=0,P1_3=1;} 	//向后转
+#define Left_moto_back    {P1_2=0,P1_3=1;}  //向后转
 #define Left_moto_Stop    {P1_2=0,P1_3=0;}  //停转
-#define Right_moto_go     {P1_4=1,P1_5=0;}	//右边电机向前走
-#define Right_moto_back   {P1_4=0,P1_5=1;}	//向后走
+#define Right_moto_go     {P1_4=1,P1_5=0;}  //右边电机向前走
+#define Right_moto_back   {P1_4=0,P1_5=1;}  //向后走
 #define Right_moto_Stop   {P1_4=0,P1_5=0;}  //停转
 
 unsigned char Left_Speed = 0; //规定速度
 unsigned char Right_Speed = 0;
 unsigned char push_val_left =0;// PWM调速，速度循环
 unsigned char push_val_right=0;// 右电机占空比N/20
-unsigned  int  time=0; //PWM时间循环
+unsigned int time=0;   //PWM时间循环
 bit Left_stop = 1;
 bit Right_stop = 1;
 
@@ -32,81 +32,81 @@ sbit EN2 = P1^7;
 sbit BUZZ = P2^3;//蜂鸣器
 
 void delay(unsigned int k){//延时k ms
-  unsigned int i;
-  unsigned char j;
-  for(i = 0;i < k; i++){
-    for(j = 0;j < 200; j++);
-    for(j = 0;j < 102; j++);
-  }
+        unsigned int i;
+        unsigned char j;
+        for(i = 0; i < k; i++) {
+                for(j = 0; j < 200; j++) ;
+                for(j = 0; j < 102; j++) ;
+        }
 }
 void frontRun() {
-  Left_Speed = 12;
-  Left_Speed = 12;
-  Left_moto_go;
-  Right_moto_go;
+        Left_Speed = 12;
+        Left_Speed = 12;
+        Left_moto_go;
+        Right_moto_go;
 }
 void backRun(){
-  Left_Speed = 12;
-  Left_Speed = 12;
-  Left_moto_back;
-  Right_moto_back;
+        Left_Speed = 12;
+        Left_Speed = 12;
+        Left_moto_back;
+        Right_moto_back;
 }
 void turnLeft(){  //左拐 右前左后
-  Left_Speed = 12;
-  Right_Speed = 15;
-  Left_moto_back;
-  Right_moto_go;
+        Left_Speed = 12;
+        Right_Speed = 15;
+        Left_moto_back;
+        Right_moto_go;
 }
 void turnRight(){
-  Right_Speed = 12;
-  Left_Speed = 15;
-  Right_moto_back;
-  Left_moto_go;
+        Right_Speed = 12;
+        Left_Speed = 15;
+        Right_moto_back;
+        Left_moto_go;
 }
 void stop(){
-  Right_moto_Stop;
-  Left_moto_Stop;
+        Right_moto_Stop;
+        Left_moto_Stop;
 }
 
 void pwm_out_left_moto(){
-  if(Left_stop){
-     if(Left_Speed<=push_val_left){
-       Left_moto_pwm=1;
-		 }
-	   else{
-	       Left_moto_pwm=0;
-		 }
-     if(Left_Speed>=20)
-	   Left_Speed=0;
-   }
-   else{
-     Left_moto_pwm=0;
-	  }
+        if(Left_stop) {
+                if(Left_Speed<=push_val_left) {
+                        Left_moto_pwm=1;
+                }
+                else{
+                        Left_moto_pwm=0;
+                }
+                if(Left_Speed>=20)
+                        Left_Speed=0;
+        }
+        else{
+                Left_moto_pwm=0;
+        }
 }
 
-   void pwm_out_right_moto(){
-     if(Right_stop){
-       if(Right_Speed<=push_val_right){
-         Right_moto_pwm=1;
-		   }
-       else{
-         Right_moto_pwm=0;
-       }
-       if(Right_Speed>=20)
-         Right_Speed=0;
-       }
-       else{
-         Right_moto_pwm=0;
-	     }
-     }
+void pwm_out_right_moto(){
+        if(Right_stop) {
+                if(Right_Speed<=push_val_right) {
+                        Right_moto_pwm=1;
+                }
+                else{
+                        Right_moto_pwm=0;
+                }
+                if(Right_Speed>=20)
+                        Right_Speed=0;
+        }
+        else{
+                Right_moto_pwm=0;
+        }
+}
 
 ///*TIMER0中断服务子函数产生PWM信号*/
- 	void timer0()interrupt 1   using 2{
-   TH0=0XFc;	  //1Ms定时
-	 TL0=0X18;
-	 time++;
-	 Left_Speed++;
-	 Right_Speed++;
-	 pwm_out_left_moto();
-	 pwm_out_right_moto();
- }
+void timer0() interrupt 1   using 2 {
+        TH0=0XFc; //1Ms定时
+        TL0=0X18;
+        time++;
+        Left_Speed++;
+        Right_Speed++;
+        pwm_out_left_moto();
+        pwm_out_right_moto();
+}
